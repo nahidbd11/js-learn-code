589dd120f4287df039d27a12693da21320578807
nahid <diegonahid@gmail.com>
2021-12-09 12:15:18 +0600

    abort event listner optinal for removeeventlistner

--- abortcontroller.js
+++ abortcontroller.js
@@ -12,11 +12,25 @@ x = fetch("https://catfact.ninja/fact", { signal })
 let btn = document.createElement("button");
 btn.textContent = "click me";
 document.body.appendChild(btn);
-let p = document.createElement("p");
-btn.onclick = function () {
-  x.then(({ fact }) => showonDom(fact));
-};
+let envetController = new AbortController();
+let evntsignal = envetController.signal;
+btn.addEventListener(
+  "click",
+  () => {
+    x.then(({ fact }) => showonDom(fact));
+    console.log("hello");
+  },
+  { signal: evntsignal }
+);
 
+let btn1 = document.createElement("button");
+btn1.textContent = "cancel click";
+document.body.appendChild(btn1);
+btn1.addEventListener("click", () => {
+  envetController.abort();
+});
+//!it works same as removeeventlistner;
+let p = document.createElement("p");
 function showonDom(fact) {
   p.textContent = fact;
   document.body.append(p);
